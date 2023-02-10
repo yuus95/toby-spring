490537f3fa1e664d4cc1169bde53790bdd671533
yushin <kkad45@naver.com>
2023-02-10 20:33:41 +0900

    feat DispatcherServlet 기초

--- src/main/java/com/example/tobyspring/TobySpringApplication.java
+++ src/main/java/com/example/tobyspring/TobySpringApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,49 +20,30 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 독립 실행형 스프링 어플리케이션
- * 서블릿 기능을 신경쓰지 않고 어떻게 개발하는지 파악하기
- * 각각의 컨트롤러를 스프링 컨테이너에서 사용하기
- * - 프론트 컨트롤러에서 직접 컨트롤러를 생산하지 않아도 된다.
- * <p>
- * 스프링 컨테이너는 오브젝트를 딱 한번만 만든다.
- * - SingleTon 과 비슷한 형태를 갖는다.
- * - 컨트롤러는 웹 컨트롤러 이기 떄문에 웹을 통해 들어온 데이터를 검증하고 비즈니스 로직 객체한테 데이터를 요청하여 받은 뒤 반환한다.
- *
- * Dependency - Injection
- *  -
+ * 문제점
+ * - 하드코딩된 매핑 작업 매번 URI 조건 분기점을 만들어야 되는가?
+ * - 파라미터 바인딩을 직접 해줘야 하는가?
+ * -
  */
 @SpringBootApplication
 public class TobySpringApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 
         // 빈으로 등록하면  스프링 컨테이너에 등록된다.
         // 빈으로 등록된 클래스들을 어덯게 의존성을 채워줄까?
         //      -  컨테이너에서 찾아서 등록해준다.
+
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         ServletWebServerFactory webServer = new TomcatServletWebServerFactory();
         webServer.getWebServer(servletContext -> {
-            servletContext.addServlet("frontController", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-
-                        // 스프링 컨테이너가 가지고 있는 오브젝트 호출
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(helloController.getString(name));
-                    } else {
-                        resp.getWriter().println("Text  " + name);
-                    }
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet",
+                    new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
         }).start();
 
     }
