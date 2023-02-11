c177d34a3473332242f91d97d21b4a54da5e8ca5
yushin <kkad45@naver.com>
2023-02-11 15:32:42 +0900

    feat servlet 과 톰캣 빈으로 등록

--- src/main/java/com/example/tobyspring/TobySpringApplication.java
+++ src/main/java/com/example/tobyspring/TobySpringApplication.java
@@ -2,7 +2,10 @@ package com.example.tobyspring;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -12,10 +15,21 @@ import org.springframework.web.servlet.DispatcherServlet;
  * 문제는 컴포넌트만을 활용하여 빈을 등록하면 어떠한 빈이 등록되었는지 파악하기 어렵다.
  * 어
  * 컴포넌트 어노테이션을 가지고 있는 하위의 어노테이션을 사용하여 레이어를 구분할 수 있다.
- *
  */
 @ComponentScan
+@Configuration
 public class TobySpringApplication {
+
+    @Bean
+    public DispatcherServlet dispatcherServlet(WebApplicationContext webApplicationContext) {
+        return new DispatcherServlet(webApplicationContext);
+    }
+
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
     public static void main(String[] args) {
         // onRefresh
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
@@ -23,10 +37,15 @@ public class TobySpringApplication {
             protected void onRefresh() {
                 super.onRefresh();
 
-                ServletWebServerFactory webServer = new TomcatServletWebServerFactory();
-                webServer.getWebServer(servletContext -> servletContext.addServlet("dispatcherServlet",
-                        new DispatcherServlet(this)
-                ).addMapping("/*")).start();
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                ServletWebServerFactory webServer = this.getBean(TomcatServletWebServerFactory.class);
+
+                dispatcherServlet.setApplicationContext(this);
+
+                webServer.getWebServer(servletContext -> servletContext
+                        .addServlet("dispatcherServlet", dispatcherServlet)
+                        .addMapping("/*")
+                ).start();
             }
         };
 
