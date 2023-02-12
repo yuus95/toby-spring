ee0bd758d4738f3e361e66eb2465bd7f341dde9e
yushin <kkad45@naver.com>
2023-02-12 16:20:08 +0900

    feat : 메타 어노테이션 적용

--- src/main/java/com/example/tobyspring/TobySpringApplication.java
+++ src/main/java/com/example/tobyspring/TobySpringApplication.java
@@ -3,6 +3,11 @@ package com.example.tobyspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 메타 어노테이션
+ *  - 메타 어노테이션을 갖고 있는 어노테이션을 사용하면 해당 메타 어노테이션을 사용하는 것과 똑같은 효과를 얻는다.
+ *  - 어노테이션에 이름을 작성하여 사용하면 해당 어노테이션이 무슨의미인지 쉽게 부여할 수 있다.
+ */
 @SpringBootApplication
 public class TobySpringApplication {
     public static void main(String[] args) {
--- src/test/java/com/example/tobyspring/SimpleHelloServiceTest.java
+++ src/test/java/com/example/tobyspring/SimpleHelloServiceTest.java
@@ -3,15 +3,25 @@ package com.example.tobyspring;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- *
  * 고립된 테스트가 가능하다.
- *
  */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test // 메타 어노테이션을 적용
+@interface UnitTest{
+
+}
 class SimpleHelloServiceTest {
-    @Test
+    @UnitTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
         String result = helloService.sayHello("Test");
