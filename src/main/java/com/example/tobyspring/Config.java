ab3dc38a20722ae03a892628943904395b25161b
yushin <kkad45@naver.com>
2023-02-12 16:34:22 +0900

    feat : 어노테이션 합성 적용

---
+++ src/main/java/com/example/tobyspring/Config.java
@@ -0,0 +1,21 @@
+package com.example.tobyspring;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class Config {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet(WebApplicationContext webApplicationContext) {
+        return new DispatcherServlet(webApplicationContext);
+    }
+}
