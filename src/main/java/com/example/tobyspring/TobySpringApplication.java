8b22bea8fd25a2a56e7de1dd4c7f1c57a4336598
yushin <kkad45@naver.com>
2023-02-24 17:27:08 +0900

    feat 서블릿 생성

--- src/main/java/com/example/tobyspring/TobySpringApplication.java
+++ src/main/java/com/example/tobyspring/TobySpringApplication.java
@@ -1,42 +1,37 @@
 package com.example.tobyspring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-import javax.annotation.PostConstruct;
-
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
- * 스프링 컨테이너가 초기화 된 이후
- * 등록된 빈을 이용하여 테스트코드 작성
- * 또는 초기화 된 이후 빈 등록
+ * HTTPie
+ * curl
  * <p>
- * 어플리케이션 외부 환겨 변수 순서
- * 시스템 환경 변수, (application.yml, properties )
+ * Response
+ * - 응답갓
+ * - contentType
+ * - 내용
  */
-@SpringBootApplication
+
 public class TobySpringApplication {
-    private final JdbcTemplate jdbcTemplate;
-
-    public TobySpringApplication(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    @PostConstruct
-    void init() {
-        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
-    }
-
-    //    @Bean
-//    ApplicationRunner applicationRunner(Environment env) {
-//        return args -> {
-//            String property = env.getProperty("my.name");
-//            System.out.println("property  " + property );
-//        };
-//    }
-
     public static void main(String[] args) {
-        SpringApplication.run(TobySpringApplication.class, args);
+        TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = webServerFactory.getWebServer(servletContext -> servletContext.addServlet("hello", new HttpServlet() {
+            @Override
+            protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                System.out.println("Hello URL Active");
+                resp.setStatus(200);
+                resp.setHeader("content-type","text");
+                resp.getWriter().println("Hello Servlet");
+            }
+        }).addMapping("/hello"));
+
+        webServer.start();
     }
 }
