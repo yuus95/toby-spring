88101d24ec7905fe0567912eb31b0768cdda85e0
yushin <kkad45@naver.com>
2023-02-25 23:23:05 +0900

    feat 프론트 컨트롤러와 핸들러 연동 작업
    -

---
+++ src/main/java/com/example/tobyspring/RestHelloController.java
@@ -0,0 +1,9 @@
+package com.example.tobyspring;
+
+public class RestHelloController {
+
+    public String hello(String name) {
+        return "hello " + name;
+    }
+
+}
--- src/main/java/com/example/tobyspring/TobySpringApplication.java
+++ src/main/java/com/example/tobyspring/TobySpringApplication.java
@@ -25,21 +25,32 @@ import java.io.IOException;
 public class TobySpringApplication {
     public static void main(String[] args) {
         TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = webServerFactory.getWebServer(servletContext -> servletContext.addServlet("hello", new HttpServlet() {
-            @Override
-            protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                    String name = req.getParameter("name");
-                    resp.setStatus(200);
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-                    resp.getWriter().println("Hello Servlet " + name);
-                }
-                else{
-                    resp.setStatus(404);
-                }
+        WebServer webServer = webServerFactory.getWebServer(servletContext -> {
+            RestHelloController helloController = new RestHelloController();
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-            }
-        }).addMapping("/*"));
+                    // 프론트 컨트롤러가 필요한 이유는 공통적으로 필요한 부분을 한번에 해결할 수 있기 떄문이다.
+                    //  - 인증 보안, 인가
+                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        // 컨트롤러단에는 자바 객체로만 전달된다 웹 기술은 서블릿에서 처리를 해준다.
+                        String result = helloController.hello(name);
+
+                        // 반환 작업일 때도 자바객체로 받고 웹 기술은 서블릿에서 활용한다.
+                        resp.setStatus(200);
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+                        resp.getWriter().println(result);
+                    }
+                    else{
+                        resp.setStatus(404);
+                    }
+
+                }
+            }).addMapping("/*");
+        });
 
         webServer.start();
     }
