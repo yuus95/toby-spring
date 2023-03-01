d9448e540a7f7b4a13505c8d7e385ecbc1b3fd18
yushin <kkad45@naver.com>
2023-03-01 20:49:49 +0900

    feat dispatcher Servlet 구현

--- src/main/java/com/example/tobyspring/TobySpringApplication.java
+++ src/main/java/com/example/tobyspring/TobySpringApplication.java
@@ -2,16 +2,8 @@ package com.example.tobyspring;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * 스프링 컨테이너를 생성하여 우리가 직접 컨트롤러를 생성하는 것이 아니라 컨테이너에게 요청을 해서 만들어준 컨트롤러를 가져와서 사용할 수 있도록 한다.
@@ -19,6 +11,12 @@ import java.io.IOException;
  * - 팩토리 메서드를 활용하여 빈 객체들을 생성하여 직접 주입할 수 있다,
  * - 생성자 주입 방식을 이용하여 빈 객체가 생성될 때 주입해줄 수도 잇다.
  *
+ *
+ * 스프링 컨테이너와 연결하는데 특화가 되어있는 DispatcherServlet 이라는 개념이 도입됨
+ * 프론트 컨트롤러의 역할 처럼 공통적인 부분을 한번에 해결할 수 있다.
+ * 스프링 컨테이너와 연결하는데 특화되어있다.
+ *
+ * applicationContext -> WebApplicationContext 의존성 객체가 변경됨
  */
 
 public class TobySpringApplication {
@@ -28,36 +26,14 @@ public class TobySpringApplication {
          * 컨테이너에 빈 등록
          * 컨테이너 Refresh()
          */
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(RestHelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
         WebServer webServer = webServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-                    // 프론트 컨트롤러가 필요한 이유는 공통적으로 필요한 부분을 한번에 해결할 수 있기 떄문이다.
-                    //  - 인증 보안, 인가
-                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-                        RestHelloController bean = applicationContext.getBean(RestHelloController.class);
-                        String result = bean.hello(name);
-                        // 컨트롤러단에는 자바 객체로만 전달된다 웹 기술은 서블릿에서 처리를 해준다.
-
-                        // 반환 작업일 때도 자바객체로 받고 웹 기술은 서블릿에서 활용한다.
-                        resp.setStatus(200);
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-                        resp.getWriter().println(result);
-                    }
-                    else{
-                        resp.setStatus(404);
-                    }
-
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("hello", new DispatcherServlet(applicationContext)).addMapping("/*");
         });
 
         webServer.start();
