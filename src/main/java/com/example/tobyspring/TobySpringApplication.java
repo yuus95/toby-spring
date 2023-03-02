11213ad6579382521846c94fe67ec249c9f1a6d0
yushin <kkad45@naver.com>
2023-03-02 19:32:06 +0900

    feat 스프링 컨테이너로 통합

--- src/main/java/com/example/tobyspring/TobySpringApplication.java
+++ src/main/java/com/example/tobyspring/TobySpringApplication.java
@@ -13,10 +13,19 @@ import org.springframework.web.servlet.DispatcherServlet;
  *
  *
  * 스프링 컨테이너와 연결하는데 특화가 되어있는 DispatcherServlet 이라는 개념이 도입됨
- * 프론트 컨트롤러의 역할 처럼 공통적인 부분을 한번에 해결할 수 있다.
- * 스프링 컨테이너와 연결하는데 특화되어있다.
+ *  - 프론트 컨트롤러의 역할 처럼 공통적인 부분을 한번에 해결할 수 있다.
+ *  - 스프링 컨테이너와 연결하는데 특화되어있다.
+ *  - DispatchServlet을 이용하기 위해서는 스프링 컨테이너 구현체를 변경해야 한다.
+ *      -applicationContext -> WebApplicationContext 의존성 객체가 변경됨
  *
- * applicationContext -> WebApplicationContext 의존성 객체가 변경됨
+ *
+ * <스프링 컨테이너로 통합>
+ * 서블릿 컨테이너를 만들고  서블릿을 초기화하는 작업을 스프링 컨테이너가 초기화되는 과정에서 일어나도록 한다.
+ *  -> 스프링부트에서 현재 이렇게 사용하고 있음.
+ *      - 템플릿 메소드: 상속을 이용하여 구현한다.
+ *      - 훅 메소드: 추상 클래스에 들어있는, 아무 일도 하지 않거나 기본 행동을 정의하는 메소드로, 서브 클래스에서 오버라이드 할 수 있습니다.
+ *      - 정리 : GenericWebApplicationContext 에서 onRefresh()  훅 메소드를 구현한다.
+ *          - 서블릿 컨테이너와 서블릿초기화하는 작업을 스프링 컨테이너가 초기화 되는 과정에 넣는 작업 진행
  */
 
 public class TobySpringApplication {
@@ -26,16 +35,19 @@ public class TobySpringApplication {
          * 컨테이너에 빈 등록
          * 컨테이너 Refresh()
          */
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = webServerFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("hello", new DispatcherServlet(this)).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(RestHelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
-
-        TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = webServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new DispatcherServlet(applicationContext)).addMapping("/*");
-        });
-
-        webServer.start();
     }
 }
